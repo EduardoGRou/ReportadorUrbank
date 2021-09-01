8b96d260d5f7ae703a5ed6355d27afda50c2f8d2
Jorge <jorgerfrancom@gmail.com>
2021-09-01 11:08:17 -0500

    viewz done

--- Reportador/principal/views.py
+++ Reportador/principal/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.db import models
+from django.shortcuts import redirect, render
+from django.views.generic import CreateView, DeleteView,ListView,UpdateView
+from django.urls import reverse_lazy
+from .forms import UserForm
+from .models import User
 
-# Create your views here.
+class UserList(ListView):
+    model= User
+    template_name='index.html'
+class UserCreate(CreateView):
+    model=User
+    form_class=UserForm
+    template_name='crear_user.html'
+    success_url=reverse_lazy('index')
+
+class UserUpate(UpdateView):
+    model=User
+    form_class=UserForm
+    template_name='crear_user.html'
+    success_url=reverse_lazy('index')
+
+class UserDelete(DeleteView):
+    model=User
+    template_name='verificacion.html'
+    success_url=reverse_lazy('index')
